38fa066419d1c0c87c3c011f152b3f6e485b7402
lditzel94 <lucianoditzel94@gmail.com>
2022-09-21 20:13:27 -0300

    Initial commit

---
+++ src/main/java/org/simulacro/Main.java
@@ -0,0 +1,5 @@
+package org.simulacro;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/simulacro/SimpleCharge.java
@@ -0,0 +1,2 @@
+package org.simulacro;public class SimpleCharge {
+}
